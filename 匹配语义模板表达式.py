bfc4d2b594277a57a189585b4a44bfd11a4f0568
DrMa <690506784@qq.com>
2019-04-12 23:53:19 +0800

    rename

--- 匹配语义模板表达式.py
+++ 匹配语义模板表达式.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019/4/11 15:44
 # @Author  : DrMa
-a="小爱<[播]放|来>[一|几]<首|曲|个>@{singer}的<歌[曲]|[流行]音乐>"
-a=a.replace(']','|>').replace('[','<')
+
+
 def plusfuhao(a):
     left_index = a.index('<')
     if a[:left_index].count('>') == 0 and a[0]!='<':
         a = '<' + a[:left_index] + '>' + a[left_index:]
     return a
-a=plusfuhao(a)
-
-def prase_patt(pattern_str):
-    pass
-
-
-
 def real_xianhou(a):
     def get_xianhou_patt(a):
         all_patt_list = []
@@ -23,9 +16,12 @@ def real_xianhou(a):
             for i in a:
                 one_patt_list.append(i)
                 if one_patt_list.count('>')==one_patt_list.count('<')and one_patt_list.count('<')>0:
-                    a=a[len(one_patt_list):]
-                    all_patt_list.append(''.join(one_patt_list))
-                    break
+                    try:
+                        a=a[len(one_patt_list):]
+                        all_patt_list.append(''.join(one_patt_list))
+                        break
+                    except:
+                        break
         return all_patt_list
     all_temp=get_xianhou_patt(a)
     all_real = []
@@ -34,11 +30,6 @@ def real_xianhou(a):
         a_all = get_xianhou_patt(a)
         all_real += a_all
     return all_real
-all_xianhou_patt=real_xianhou(a)
-print(all_xianhou_patt)
-
-quit()
-
 
 def contentMerge(v1,v2):#追加模式
     #v1中的每一个字符串后追加v2中字符的内容,v1,v2是个list，elem是string的pattern
@@ -50,6 +41,73 @@ def contentMerge(v1,v2):#追加模式
             for j in v2:#v2代表当前步所有的pattern
                 results.append(i+j)
         return results
+# a=plusfuhao(a)
+
+
+def prase_patt(pattern_str):
+    if pattern_str.count('<')==1and pattern_str.count('>')==1:
+        if pattern_str.count('|'):
+            real_result=[]
+            temp_list=pattern_str.replace('<','').replace('>','').split('|')
+            for i in temp_list:
+                if i!='':
+                    real_result.append(i)
+            return real_result
+        else:
+            real_result = []
+            temp_list=[pattern_str.replace('<', '').replace('>', '')]
+            for i in temp_list:
+                if i!='':
+                    real_result.append(i)
+            return real_result
+    elif pattern_str.count('<')==0 and pattern_str.count('>')==0:
+        if pattern_str.count('|'):
+            real_result=[]
+            temp_list=pattern_str.split('|')
+            for i in temp_list:
+                if i!='':
+                    real_result.append(i)
+            return real_result
+        else:
+            real_result = []
+            if pattern_str!='':
+                real_result=[pattern_str]
+            return real_result
+    else:
+        #把最外层的|干掉
+        ors = []
+        depth = 0  # 当前级别
+        for i in range(len(pattern_str)):
+            c=pattern_str[i]
+            if c == '<':  # 因为成对出现，最后肯定depth又变成的；
+                depth += 1
+            elif c == '>' :
+                depth -= 1
+            elif c == '|' and depth == 0:
+                ors.append(i)  # 与根同层的|符号，直接拆分为子pattern处理
+        start = 0
+        result=[]
+        if len(ors) > 0:  # 包含与根同层的或符号，直接拆分处理
+            for i in range(len(ors)):
+                end = ors[i]
+                part = prase_patt(pattern_str[start:end])  # 递归了，
+                start = end + 1  # |位置的下一个位置开始
+                result.extend(part)  # 因为result和part同级，我们用extend
+        else:#说明没有大的并集，只有先后,这一部分出现问题
+            all_xianhou_patt = real_xianhou(pattern_str)
+            #['<一|>', '<df>']没有问题
+            for i in range(len(all_xianhou_patt)):
+                result = contentMerge(result, prase_patt(all_xianhou_patt[i]))
+        return result
+
+a="[一]<df>|ji"
+print('原始    ',a)
+a=a.replace(']','|>').replace('[','<')
+print('修改后    ',a)
+print(prase_patt(a))
+quit()
+
+
 def analysePattern(pattern,rootDepth,addEmpty):
     '''
     string: pattern
