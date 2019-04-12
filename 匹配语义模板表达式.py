105195b3dd0e7b34b8c8d5229100e2050e0d6de9
DrBugKiller <32807279+DrBugKiller@users.noreply.github.com>
2019-04-12 09:31:59 +0800

    19.4.12

--- 匹配语义模板表达式.py
+++ 匹配语义模板表达式.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019/4/11 15:44
 # @Author  : DrMa
+a="<[播]放|来>[一|几]<首|曲|个>@{singer}的<歌[曲]|[流行]音乐>"
+a=a.replace(']','| >').replace('[','<')
+print(a)
 def contentMerge(v1,v2):#追加模式
     #v1中的每一个字符串后追加v2中字符的内容,v1,v2是个list，elem是string的pattern
     if len(v1)<1:
@@ -16,32 +19,40 @@ def analysePattern(pattern,rootDepth,addEmpty):
     string: pattern
     int: rootDepth
     bool: addEmpty
+    "<[播]放|来>[一|几]<首|曲|个>@{singer}的<歌[曲]|[流行]音乐>"
     '''
     result=[]
     if addEmpty:#是否可选，如果可选，增加‘’，这是【】的针对方法
+        # 注意，这个平级的append，就是在一个<>中
         result.append('')
-    ors=[]#存储‘或’符号的位置
-    length=len(pattern)
-    depth=rootDepth
+
+    #存储最外层的|的位置，比如：<A>[B]|<C>[D]|<E>这个或就是与根同层的，我们切分<A>[B]和<C>[D]和<E>三个合并append就可以了
+    #这里的小trick就是<>[]是成对出现之后的|符号才是与根同级。
+    ors=[]
+    length=len(pattern)#拿到pattern长度
+    depth=rootDepth#当前级别
     for i in range(length):
         c = pattern[i]#char
-        if c=='<'or c=='[':depth+=1
-        elif c=='>' or c==']':depth-=1
-        elif c=='|'and depth==rootDepth:ors.append(i)#与根同层的或符号，直接拆分为子pattern处理
+        if c=='<'or c=='[':#因为成对出现，最后肯定depth又变成的；
+            depth+=1
+        elif c=='>' or c==']':
+            depth-=1
+        elif c=='|'and depth==rootDepth:
+            ors.append(i)         #与根同层的|符号，直接拆分为子pattern处理
     start=0
     if len(ors)>0:#包含与根同层的或符号，直接拆分处理
         for i in range(len(ors)):
             end=ors[i]
-            part=analysePattern(pattern[start:end-start],rootDepth,False)#list part
-            start=end+1
-            result.append(part[0])
-            result.append(part[-1])
-    else:
+            part=analysePattern(pattern[start:end],rootDepth,False)#递归了，
+            start=end+1#|位置的下一个位置开始
+            result.extend(part)#因为result和part同级，我们用extend
+    else:#此时没有最外的|,此时没有同级的，只有先后级别
         depth=rootDepth
-        stacks=[]#存放int的栈
+        stacks=[]#存放int的栈，把<和[的位置记录下来
         hasPattern=False#是否包含标记符| <> []
         necessary_Start=0#标记形如"歌[曲]"或"[歌]曲"的情况，他们等同于"<歌>[曲]"或"[歌]<曲>"
-        for i in range(length):
+        #"<[播]放|来>[一|几]<首|曲|个>@{singer}的<歌[曲]|[流行]音乐>"
+        for i in range(length):#开始遍历pattern的string
             c=pattern[i]
             if c=='<'or c=='[':
                 if depth==rootDepth:
@@ -49,7 +60,7 @@ def analysePattern(pattern,rootDepth,addEmpty):
                         part=analysePattern(pattern[necessary_Start:i-necessary_Start],depth,False)
                         result=contentMerge(result,part)
                 hasPattern=True
-                depth+=1
+                depth+=1#深度+1
                 stacks.append(i)
             elif c=='>':
                 depth-=1
@@ -85,6 +96,81 @@ def isMatch(set,query):
     for i in range(len(set)):
         if query==set[i]:return True
     return False
-allExpress=analysePattern("<[播]放|来>{singer}的",0, False)
-print('Print ALL Expression')
-print(allExpress)
+
+
+
+def inprocess(strings):
+    dicts = {'<':'>', '>':'<', '[':']', ']':'[' }
+
+    flag = []
+    i = 0
+    res = []
+    start = 0
+    # split inputs to many strings
+    while i <len(strings):
+        s = strings[i]
+        if s=='<' or s=='[':
+            flag.append(s)
+            if strings[start]=='@':
+                res.append(strings[start:i])
+                start = i
+
+        elif flag and s==dicts[flag[-1]]:
+            tmp = flag.pop()
+            if flag:
+                continue
+            else:
+                res.append(strings[start+1:i])
+                start = i+1
+            del tmp
+
+        i += 1
+
+    # process the res
+    collect = []
+    for id, lists in enumerate(res):
+        if '|' not in lists:
+            collect.append(lists)
+            continue
+
+        lists = lists.split('|')
+        for s in lists:
+            i = 0
+            # while i < len(s):
+            if s[i]=='[':
+                collect.append(s[i+1:s.index(']')])
+                i = s.index(']')+1
+                if i<len(s):
+                    collect.append(collect[-1]+s[i:])
+
+            elif '[' not in s:
+                collect.append(s)
+    # for c in res:
+    #     print(c)
+    print(collect)
+    return collect
+
+def testing(qurry, collect):
+    i = 0
+    while i<len(qurry):
+        c = qurry[i]
+        if c in collect:
+            collect = collect[collect.index(c)+1:]
+        elif c=='@':
+            if qurry[i:qurry.index('}')+2] in collect:
+                i = qurry.index('}') + 1
+            else:
+                return 0
+        else:
+            return 0
+        i += 1
+    return 1
+
+
+if __name__ == '__main__':
+    inputs = "<[播]放|来>[一|几]<首|曲|个>@{singer}的<歌[曲]|[流行]音乐>"
+    res = inprocess(inputs)
+
+    qurry = "LJS@{singer}GLXGQ"
+
+
