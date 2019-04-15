38b3b158a005a616b4710a609488218de5a4b619
DrBugKiller <32807279+DrBugKiller@users.noreply.github.com>
2019-04-15 21:55:09 +0800

    verNo2

--- 匹配语义模板表达式.py
+++ 匹配语义模板表达式.py
@@ -40,44 +40,46 @@ def analysePattern(pattern,rootDepth):
     if len(ors)>0:#包含与根同层的或符号，直接拆分处理
         for i in range(len(ors)):
             end=ors[i]
-            print(pattern[start:end])
+            print('pattern[start:end]', pattern[start:end], rootDepth)
             part=analysePattern(pattern[start:end],rootDepth)#递归了，
             start=end+1#|位置的下一个位置开始
             result+=part#因为result和part同级，我们用extend
+        print('pattern[start:end]', pattern[start:], rootDepth)
         part = analysePattern(pattern[start:], rootDepth)
         result+=part
     else:#此时没有最外的|,此时没有同级的，只有先后级别
         depth=rootDepth
-        stacks=[] #存放int的栈，把<和[的位置记录下来
-        hasPattern=False #是否包含标记符| <> []
-        necessary_Start=0 #标记形如"歌[曲]"或"[歌]曲"的情况，他们等同于"<歌>[曲]"或"[歌]<曲>"
+        left_bracket_indexs=[] #把<位置记录下来,遇到>我们
+        hasPattern=False #是否包含标记符<
+        right_bracket_next=0 #记录同根级别的‘>’的下一个位置。这是这个代码中最出彩的地方。标记形如"歌[曲]"或"[歌]曲"的情况，他们等同于"<歌>[曲]"或"[歌]<曲>"
 
         for i in range(length):#开始遍历pattern的string
             c=pattern[i]
             if c=='<':
                 if depth==rootDepth:
-                    if i>necessary_Start:#把中间没有<>但是必选的内容放进去
-                        print('pattern[necessary_Start:i]:',pattern[necessary_Start:i])
-                        part=analysePattern(pattern[necessary_Start:i],depth)
+                    if i-right_bracket_next>0:#把中间没有<>但是必选的内容放进去
+                        print('pattern[necessary_Start:i]:',pattern[right_bracket_next:i],depth)
+                        part=analysePattern(pattern[right_bracket_next:i],depth)
                         result=contentMerge(result,part)
                 hasPattern=True
                 depth+=1#深度+1
-                stacks.append(i)
-            elif c=='>':
+                left_bracket_indexs.append(i)
+            elif c=='>':#此时的'>'可能是同根级别，也可能更第一级
                 depth-=1
                 if depth==rootDepth:
-                    necessary_Start=i+1
-                index=stacks.pop()
+                    right_bracket_next=i+1#代表‘>’的下一个位置
+                left_bracket_index=left_bracket_indexs.pop()#不管是否同根级别，都要pop，以保证<>的级别相同
+                #只有当'>'是同根级别才递归
                 if depth==rootDepth:
-                    print('pattern[index+1:i]:',pattern[index+1:i])
-                    part=analysePattern(pattern[index+1:i],depth+1)#看一下这个
+                    print('pattern[index+1:i]:',pattern[left_bracket_index+1:i],depth)
+                    part=analysePattern(pattern[left_bracket_index+1:i],depth+1)#看一下这个
                     result=contentMerge(result,part)
         if hasPattern==False:#<>|[]都没有，那就直接把pattern放进去
             result.append(pattern)
         else:
-            if necessary_Start<len(pattern):#处理形如<A|B>C的C这一部分
-                print('pattern[necessary_Start:len(pattern)]:',pattern[necessary_Start:len(pattern)])
-                part=analysePattern(pattern[necessary_Start:len(pattern)],depth)
+            if right_bracket_next<len(pattern):#处理形如<A|B>C的C这一部分
+                print('pattern[necessary_Start:len(pattern)]:',pattern[right_bracket_next:len(pattern)],depth)
+                part=analysePattern(pattern[right_bracket_next:len(pattern)],depth)
                 result=contentMerge(result,part)
     return result
 def del_square_brackets(str_input):
@@ -85,10 +87,10 @@ def del_square_brackets(str_input):
     return str_output
 test_str='<<A|B>C>D'
 test_str=del_square_brackets(test_str)
-inputs = "小爱同学，<[播]放|来>[一|几]<首|曲|个>周杰伦的<歌[曲]|[流行]音乐>好吗？"
+inputs = "小爱同学<[播]放|来>[一|几]<首|曲|个>周杰伦的<歌[曲]|[流行]音乐>好吗？"
 inputs=del_square_brackets(inputs)
+print(inputs)
 all_ex=set(analysePattern(inputs,0))
-print(test_str)
 print(all_ex)
 
 
