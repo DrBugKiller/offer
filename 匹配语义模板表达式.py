cd6f9d998c3865b9078f9dacfecc02b5dcb4c3a9
DrBugKiller <32807279+DrBugKiller@users.noreply.github.com>
2019-04-15 16:49:39 +0800

    verNo2

--- 匹配语义模板表达式.py
+++ 匹配语义模板表达式.py
@@ -1,65 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019/4/11 15:44
 # @Author  : DrMa
-def get_nm_index(pattern_str):#拿到中间同根级别的没有<>的选项
-    ors = []
-    depth = 0  # 当前级别
-    for i in range(len(pattern_str)):
-        c = pattern_str[i]
-        if c == '<':  # 因为成对出现，最后肯定depth又变成的；
-            depth += 1
-        elif c == '>':
-            depth -= 1
-        elif c != '|' and depth == 0 and(pattern_str[i-1]=='>'or pattern_str[i+1]=='<'):
-            ors.append(i)  # 与根同层的|符号，直接拆分为子pattern处理
-    return ors
+
 #处理先后的时候把根级别的没有<>的考虑进去
-def plus_symbol(a):#在跟级别上加上'<>':  'hello<A|B>nothing<C|D>what'
-    left_index = a.index('<')
-    #左边的处理掉
-    if a[:left_index].count('>') == 0 and a[0]!='<':
-        a = '<' + a[:left_index] + '>' + a[left_index:]
-    #右边的的处理掉
-    temp=a[::-1]
-    left_index=temp.index('>')
-    if temp[:left_index].count('<') == 0 and temp[0]!='>':
-        temp = '>' + temp[:left_index] + '<' + temp[left_index:]
-    a=temp[::-1]
-    #最难的中间，有多个同根级别
-    index_nm=get_nm_index(a)
-    index_list=[]
-    temp_int=0#一旦插入，整体长度变长，那么下标需要+2
-    for i in range(0,len(index_nm),2):
-        index_list.append([index_nm[i]+temp_int,index_nm[i+1]+temp_int])
-        temp_int+=2
-    for i in index_list:
-        patt_list=list(a)
-        patt_list.insert(i[0],'<')
-        patt_list.insert(i[1]+2,'>')
-        a=''.join(patt_list)
-    return a
-def real_xianhou(a):
-    def get_xianhou_patt(a):
-        all_patt_list = []
-        while len(a)>0:
-            one_patt_list = []
-            for i in a:
-                one_patt_list.append(i)
-                if one_patt_list.count('>')==one_patt_list.count('<')and one_patt_list.count('<')>0:
-                    try:
-                        a=a[len(one_patt_list):]
-                        all_patt_list.append(''.join(one_patt_list))
-                        break
-                    except:
-                        break
-        return all_patt_list
-    all_temp=get_xianhou_patt(a)
-    all_real = []
-    for i in all_temp:
-        a = plus_symbol(i)
-        a_all = get_xianhou_patt(a)
-        all_real += a_all
-    return all_real
 def contentMerge(v1,v2):#追加模式
     #v1中的每一个字符串后追加v2中字符的内容,v1,v2是个list，elem是string的pattern
     if len(v1)<1:
@@ -70,18 +13,7 @@ def contentMerge(v1,v2):#追加模式
             for j in v2:#v2代表当前步所有的pattern
                 results.append(i+j)
         return results
-def ifnest(pattern_str):
-    left_num=0
-    nest_flag=False
-    for i in pattern_str:
-        if i=='<':
-            left_num+=1
-        elif i=='>':
-            left_num-=1
-        if left_num>1:
-            nest_flag=True
-            break
-    return nest_flag
+
 def get_or_index(pattern_str):
     ors = []
     depth = 0  # 当前级别
@@ -94,7 +26,6 @@ def get_or_index(pattern_str):
         elif c == '|' and depth == 0:
             ors.append(i)  # 与根同层的|符号，直接拆分为子pattern处理
     return ors
-
 def analysePattern(pattern,rootDepth,addEmpty):
     '''
     string: pattern
@@ -129,8 +60,7 @@ def analysePattern(pattern,rootDepth,addEmpty):
         #"<[播]放|来>[一|几]<首|曲|个>@{singer}的<歌[曲]|[流行]音乐>"
         for i in range(length):#开始遍历pattern的string
             c=pattern[i]
-
-            if c=='<'or c=='[':
+            if c=='<':
                 if depth==rootDepth:
                     if i>necessary_Start:#把中间没有<>但是必选的nothing放进去
                         part=analysePattern(pattern[necessary_Start:i],depth,False)
@@ -147,14 +77,14 @@ def analysePattern(pattern,rootDepth,addEmpty):
                     print(pattern[index+1:i])
                     part=analysePattern(pattern[index+1:i],depth+1,False)#看一下这个
                     result=contentMerge(result,part)
-            elif c==']':
-                depth-=1
-                if depth==rootDepth:
-                    necessary_Start=i+1
-                index = stacks.pop()
-                if depth==rootDepth:
-                    part=analysePattern(pattern[index+1:i],depth+1,True)
-                    result=contentMerge(result,part)
+            # elif c==']':
+            #     depth-=1
+            #     if depth==rootDepth:
+            #         necessary_Start=i+1
+            #     index = stacks.pop()
+            #     if depth==rootDepth:
+            #         part=analysePattern(pattern[index+1:i],depth+1,True)
+            #         result=contentMerge(result,part)
         if hasPattern==False:#<>|[]都没有，那就直接把pattern放进去
             result.append(pattern)
         else:
@@ -171,9 +101,14 @@ def isMatch(set,query):
     for i in range(len(set)):
         if query==set[i]:return True
     return False
-test_str='[D|E]'
+def del_square_brackets(str_input):
+    str_output=str_input.replace('[','<').replace(']','|>')
+    return str_output
+test_str='<<A|B>C>D'
+test_str=del_square_brackets(test_str)
 inputs = "<[播]放|来>[一|几]<首|曲|个>@{singer}的<歌[曲]|[流行]音乐>"
-all_ex=analysePattern(test_str,0,False)
+inputs=del_square_brackets(inputs)
+all_ex=set(analysePattern(test_str,0,False))
 print(test_str)
 print(all_ex)
 
