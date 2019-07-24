3d28dc03e408ff6cba5d2362b689571d46f55d42
马浩洋 <ma_hao_yang@163.com>
2019-07-24 21:39:13 +0800

    添加最小生成树

--- 剑指offer/第二章面试需要的基础知识/8-二叉树的下一个结点.py
+++ 剑指offer/第二章面试需要的基础知识/8-二叉树的下一个结点.py
@@ -3,7 +3,15 @@
 # @Author  : DrMa
 '''
 题目8-二叉树的下一个结点:
+给定一棵二叉树和其中的一个节点,如何找出中序遍历序列的下一个节点?
+树中的节点除了有两个分别指向左,右子节点的指针,还有一个指向父节点的指针.
 思路:
+我们分3种节点考虑:
+1. 有右子节点或右子树: 那么一直找它的左子树, 直到没有左子树, Tree_node.left=None
+2. 没有右子节点或右子树, 但是父节点的左子节点: 那么它的父节点就是它下一个节点.
+3. 没有右子节点或右子树, 但是父节点的右子节点: 那么向上找父节点, 直到找到一个父节点,
+                                            这个父节点是父父节点的左子树, 就返回
+
 '''
 class TreeLinkNode:
     def __init__(self, x):
@@ -11,21 +19,18 @@ class TreeLinkNode:
         self.left = None
         self.right = None
         self.parent = None#指向父节点的
-#这个应该是双亲孩子表示法
 def GetNext(pNode):
-    # write code here
     if not pNode:#pNode是None就返回None
         return pNode
-    if pNode.right:#if pNode.right!=None:如果一个节点有右子树,那么它的下一个节点是右子树中最左子节点
+    #如果一个节点有右子树,那么它的下一个节点是右子树中最左子节点
+    if pNode.right:
         temp=pNode.right
         while temp.left:
                temp=temp.left
         return temp
-    #一个节点没有右孩子
-    while pNode.parent:#一直到pNode是根节点,那么pNode.parent是None
-        if pNode==pNode.parent.left:#如果该节点是其父节点的左孩子，则返回父节点；否则继续向上遍历其父节点的父节点，重复之前的判断，返回结果。
+    #如果pNode不是左子树,往上走,一直找到左子树的跟节点.
+    while pNode.parent:
+        if pNode==pNode.parent.left:
             return pNode.parent
-        #如果pNode不是左子树,往上走,一直找到左子树的跟节点.
         pNode=pNode.parent#如果pNode是根节点就结束
     return pNode
-#pNode是TreeLinkNode类, 有4个属性值, 除了根节点是数值, 其他都是子树或者父母树类
--- 剑指offer/第二章面试需要的基础知识/9-用两个栈实现队列.py
+++ 剑指offer/第二章面试需要的基础知识/9-用两个栈实现队列.py
@@ -4,25 +4,27 @@
 '''
 题目9：
 用两个stack完成一个queue的功能
-tips:
-1.python自带的pop作用是删除最后(最右边)一个元素。
-1.1在python中list的append相当于在栈中push，pop就是正常操作--把最后append的元素删除并返回
+Tips:
+1.python自带的pop作用是删除最后(最右边)一个元素并返回其值。
 2.python的list中，如果当做栈来看的话，左边属于栈底，右边属于栈顶，append和pop都是在右边操作的。
-3.此题的核心就是：push是利用stack1完成的，pop是利用stack1先把元素pop后push到stack2然后pop
+3.此题的核心就是：push是利用stack1完成的，pop是利用stack1先把元素pop后push到stack2然后pop.
 '''
-class Solution:
+class queue(object):
     def __init__(self):
         self.stack1 = []
         self.stack2 = []
     def push(self, node):
         self.stack1.append(node)
     def pop_(self):
-        if len(self.stack2):#如果stack2有东西，直接把栈顶的pop出来即可
+        # 如果stack2还有东西，说明之前已经pop过,接着pop即可.
+        if len(self.stack2)>0:
             return self.stack2.pop()
-        while(self.stack1):#如果stack2空了，我们需要从stack1先pop出来然后push（append）到stack2中去
+        # 如果stack2没有东西, 说明这是第一次pop, 先把stack1所有的都放到stack2中,
+        # 再stack2.pop()即可
+        while(self.stack1):
             self.stack2.append(self.stack1.pop())
         return self.stack2.pop()
-a=Solution()
+a=queue()
 for i in range(3):
     a.push(i)
 print(a.pop_())
